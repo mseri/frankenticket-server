0e0d442cd0123f72022f389e0059a2078d3d05a2
Mattia Asti <mattia.asti@gmail.com>
2015-09-25 22:58:54 +0100

    added routes POST users, GET users

--- routes/getUsers.js
+++ routes/getUsers.js
@@ -1,4 +1,16 @@
 'use strict';
+var db = require('../db.js');
+
 module.exports = function(request, reply) {
-  reply();
+  db.User.findAll().then(function(res){
+    var users = [];
+    res.forEach(function(e){
+      users.push({
+        id: e.dataValues.id,
+        fullName: e.dataValues.fullName,
+        phone: e.dataValues.phone
+      });
+    });
+    reply(users);
+  });
 };
--- routes/postUser.js
+++ routes/postUser.js
@@ -4,10 +4,9 @@ var db = require('../db.js');
 module.exports = function(request, reply) {
   // TODO add validation
   var user = request.payload;
-  console.log(db.User);
   db.User.create(user).then(function(query) {
     console.log(query);
     // return the ID so the frontend knows the user ID just created
-    reply(query.dataValues.id);
+    reply(query.dataValues.id).code(201);
   });
 };
