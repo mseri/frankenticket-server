9509b35c6e387bc3377e4ebe96c2f99fd5cef51d
Mattia Asti <mattia.asti@gmail.com>
2015-09-25 21:53:49 +0100

    added environment PORT

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var Hapi = require('hapi');
 var routes = require('./routes.js');
 
 var server = new Hapi.Server();
-server.connection({ port: 3000 });
+server.connection({ port: process.env.PORT || 3000 });
 
 for (var i in routes) {
   server.route(routes[i]);
