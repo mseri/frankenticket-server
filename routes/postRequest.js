9f7e6a38600357316264b44ca24e62f51a0d3824
Federico Rampazzo <frampone@gmail.com>
2015-09-26 22:01:20 +0100

    Optional payments info and Braintree fixes

--- routes/postRequest.js
+++ routes/postRequest.js
@@ -5,11 +5,15 @@ var db = require('../db.js')
 
 module.exports = function(request, reply) {
   var body = request.payload
-  braintree.createCustomer(body, function(err, user) {
+  if (!body.payment_method_nonce) {
+    return createUser(null, body)
+  }
+  braintree.createCustomer(body, createUser)
+  
+  function createUser(err, user) {
     if (err) throw err
     db.Request.create(user).then(function(query) {
-      reply(query.dataValues.id).code(201);
-    });
-  })
+      reply(query.dataValues.id).code(201)
+    })
+  }
 }
- 
