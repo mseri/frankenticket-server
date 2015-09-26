4e340a37657ea533a84a1694f235ebc885e0f5f0
Mattia Asti <mattia.asti@gmail.com>
2015-09-26 18:38:03 +0100

    authentication and websocket working fine

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ var routes = require('./routes.js');
 var db = require('./db.js').sequelize;
 var BTAPI = require('./lib/bthack.js');
 var WebSocket = require('ws');
+
 var ws;
+
+function wsConnect() {
+  ws = new WebSocket(BTAPI.state.socketServer + "/" + BTAPI.state.sessionKey);
+}
 
 var server = new Hapi.Server();
 server.connection({
@@ -22,16 +27,20 @@ db.sync().then(function() {
   });
 
   BTAPI.auth()
+    .then(wsConnect)
     .then(function() {
-      ws = new WebSocket(BTAPI.state.socketServer + BTAPI.state.sessionKey);
       ws.on('message', function(message) {
         console.log(JSON.stringify(JSON.parse(message.data), null, "\t") + "\n\n");
       });
       ws.on('open', function() {
         console.log('websocket connected');
       });
+
+      server.start(function() {
+        console.log('Server running at:', server.info.uri);
+      });
     })
     .catch(function(err) {
-      console.log(err);
+      console.log('Error 0', err);
     });
 });
