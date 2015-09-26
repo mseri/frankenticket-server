caf612879ad601129383025a7b33f0d98faab5b7
Marcello Seri <marcello.seri@gmail.com>
2015-09-26 23:57:50 +0100

    Cleaned index

--- index.js
+++ index.js
@@ -4,13 +4,6 @@ var Hapi = require('hapi');
 var routes = require('./routes.js');
 var db = require('./db.js').sequelize;
 var BTAPI = require('./lib/bthack.js');
-var WebSocket = require('ws');
-
-var ws;
-
-function wsConnect() {
-  ws = new WebSocket(BTAPI.state.socketServer + "/" + BTAPI.state.sessionKey);
-}
 
 var server = new Hapi.Server();
 server.connection({
